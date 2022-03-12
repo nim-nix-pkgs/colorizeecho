6cf661680e952185cf4e6d2169e6ff99351b3362
hugosenari <hugosenari@gmail.com>
2022-03-12 01:19:02 -0300

    chore: re index colorizeecho

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''colorizeEcho is a package which colorize echo message on Windows command prompt.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."colorizeecho-main".url = "path:./main";
+  inputs."colorizeecho-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."colorizeecho-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ main/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''colorizeEcho is a package which colorize echo message on Windows command prompt.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-colorizeEcho-main.flake = false;
+  inputs.src-colorizeEcho-main.owner = "s3pt3mb3r";
+  inputs.src-colorizeEcho-main.ref   = "refs/heads/main";
+  inputs.src-colorizeEcho-main.repo  = "colorizeEcho";
+  inputs.src-colorizeEcho-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-colorizeEcho-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-colorizeEcho-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
